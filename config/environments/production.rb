89446b09b2632443548df3c4ed07963936f28534
vagrant <vagrant@debian9.localdomain>
2019-04-25 18:06:11 +0900

    [others] Mail Settings of Production Environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
-    domain: "enigmatic-plateau-82988.herokuapp.com",
+    domain: "heroku.com",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
