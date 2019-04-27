af9d30c2fefcee1bb4a87f1dda59c6d4b7bd86df
vagrant <vagrant@debian9.localdomain>
2019-04-28 00:24:24 +0900

    [others] change sender name of email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,11 +92,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "enigmatic-plateau-82988.herokuapp.com" }
   #config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
-    domain: "enigmatic-plateau-82988.herokuapp.com",
+    domain: "heroku.com",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "info@enigmatic-plateau-82988.herokuapp.com"
+  config.mailer_sender = "info@glacial-shelf-81220.herokuapp.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
